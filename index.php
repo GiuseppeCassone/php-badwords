34326827c7c97b1c0a346e24a07a83f3dcecbd28
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-03-15 18:05:24 +0100

    inserito il form per chiedere all'utente di inserire un paragrafo e poi una parola da censurare

--- index.php
+++ index.php
@@ -3,11 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>PHP-BADWORDS</title>
 </head>
 <body>
-    <h1>Ciao</h1>
+    <h1 class="text-center">Form di censura</h1>
+
+    <form action="results.php" method="get">
+        <label for="paragraph">Paragrafo:</label><br>
+        <textarea id="paragraph" name="paragraph"></textarea><br>
+        <label for="word">Parola da censurare:</label><br>
+        <input type="text" id="word" name="word"><br>
+        <input type="submit" value="Invia">
+    </form>
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
